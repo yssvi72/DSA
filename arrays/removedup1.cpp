cb2ae2ad70d3da1b36ceb06be8a7780ee97da8e9
yssvi72 <141433982+yssvi72@users.noreply.github.com>
2023-09-22 22:40:57 +0530

    Update removedup1.cpp

--- arrays/removedup1.cpp
+++ arrays/removedup1.cpp
@@ -1,1 +1,9 @@
+                                          # Remove Duplicates from Sorted Array
+Removing duplicate is a tedious task if the array is unsorted but it becomes a cakewalk if the array is already sorted:
+
+  1....Naive Approach: (obvious approach)
+      If the array is already sorted , the two consecutive position will have its duplicates 
+      For eg: arr1[]={ 4,1,4,3,5,6,9,2}
+      If we sort the arr1 we get={ 1,2,3,4,4,5,6,9} , now by looking at this we can say that 4 is repeated twice so delete it. 
+      Program :
 
