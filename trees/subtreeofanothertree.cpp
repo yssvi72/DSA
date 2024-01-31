02a5042401d28101cb957234a82b066aa0fdea60
yssvi72 <141433982+yssvi72@users.noreply.github.com>
2024-01-31 23:47:12 +0530

    Create subtreeofanothertree.cpp

---
+++ trees/subtreeofanothertree.cpp
@@ -0,0 +1,3 @@
+// A subtree of binary tree is a tree that consistes of node in tree and all nodes descendant. The tree could be considered as subtree of itself
+//If input: root=[3,4,5,1,2] and subroot=[4,1,2]  the output is true
+//You dont have to print subtree just have to compare root with subroot and displays ans in true or false
