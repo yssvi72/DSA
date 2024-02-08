c1d391593c5f05f699ef0cec304c654ab986d414
yssvi72 <141433982+yssvi72@users.noreply.github.com>
2024-02-08 23:26:11 +0530

    Create treeisomorphic.cpp

---
+++ trees/treeisomorphic.cpp
@@ -0,0 +1,43 @@
+//wo trees are called isomorphic if one of them can be obtained from other by a series of flips, i.e. by swapping left and right children of a number of nodes. 
+//Any number of nodes at any level can have their children swapped. Two empty trees are isomorphic. For example, following two trees are isomorphic with following sub-trees flipped: 2 and 3, NULL and 6, 7 and 8.
+/* A binary tree node has data, pointer to left and right children */
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+ 
+/* Given a binary tree, print its nodes in reverse level order */
+bool isIsomorphic(node* n1, node *n2)
+{
+ // Both roots are NULL, trees isomorphic by definition
+ if (n1 == NULL && n2 == NULL)
+    return true;
+ 
+ // Exactly one of the n1 and n2 is NULL, trees not isomorphic
+ if (n1 == NULL || n2 == NULL)
+    return false;
+ 
+ if (n1->data != n2->data)
+    return false;
+ 
+ // There are two possible cases for n1 and n2 to be isomorphic
+ // Case 1: The subtrees rooted at these nodes have NOT been "Flipped".
+ // Both of these subtrees have to be isomorphic, hence the &&
+ // Case 2: The subtrees rooted at these nodes have been "Flipped"
+ return
+ (isIsomorphic(n1->left,n2->left) && isIsomorphic(n1->right,n2->right))||
+ (isIsomorphic(n1->left,n2->right) && isIsomorphic(n1->right,n2->left));
+}
+/* Helper function that allocates a new node with the given data and NULL left and right pointers. */
+node* newNode(int data)
+{
+    node* temp = new node;
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+ 
+    return (temp);
+}
+ 
