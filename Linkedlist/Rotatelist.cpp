b4d775cbb29e1e6dc7cea767a48d2b95f582bb85
yashsvi <141433982+yssvi72@users.noreply.github.com>
2024-03-02 23:06:39 +0530

    Update Rotatelist.cpp

--- Linkedlist/Rotatelist.cpp
+++ Linkedlist/Rotatelist.cpp
@@ -1,1 +1,31 @@
-
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode *slow=head,*fast=head;
+        int len=0;
+        while(slow!=NULL){
+            len++;
+            slow=slow->next;
+        }
+        k=k%len;
+        slow=head;
+        while(k--){
+            fast=fast->next;
+        }
+        cout<<fast->val<<endl;
+        if(fast==NULL){
+            return head;
+        }
+        while(fast->next){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        fast->next=head;
+        head=slow->next;
+        slow->next=NULL;
+        return head;       
+    }
+};
