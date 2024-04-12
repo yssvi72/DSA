a8f370d0becfe09d5b08162f405b834a2d4a8481
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-04-12 23:20:44 +0530

    Update trappingrainwater.cpp

--- dp/trappingrainwater.cpp
+++ dp/trappingrainwater.cpp
@@ -374,6 +374,33 @@ int main()
 	int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }; 
 	int N = sizeof(arr) / sizeof(arr[0]); 
 
+
 	cout << maxWater(arr, N); 
 	return 0; 
 } 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	LEETCODE SOLUTION:
+	class Solution {
+public:
+    int trap(vector<int>& height) {
+         if (height.size() <= 2) return 0;
+        int n = height.size(), maxLeft = height[0], maxRight = height[n-1];
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            if (maxLeft < maxRight) {
+                if (height[left] > maxLeft)
+                    maxLeft = height[left];
+                else
+                    ans += maxLeft - height[left];
+                left += 1;
+            } else {
+                if (height[right] > maxRight)
+                    maxRight = height[right];
+                else
+                    ans += maxRight - height[right];
+                right -= 1;
+            }
+        }
+        return ans;
+    }
+};
