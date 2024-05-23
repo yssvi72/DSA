4ae99dc1167ab367dd3e08b0a8f84bb270fcd14f
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-05-23 19:40:21 +0530

    Create beautifulsubsets.cpp

---
+++ arrays/beautifulsubsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int _beautifulSubsets(vector<int>& nums, int k, unordered_map<int, int>& freq, int i) {
+        if (i == nums.size()) { // base case
+            return 1;
+        }
+        int result = _beautifulSubsets(nums, k, freq, i + 1); // nums[i] not taken
+        if (!freq[nums[i] - k]) { // check if we can take nums[i]
+            freq[nums[i]]++;
+            result += _beautifulSubsets(nums, k, freq, i + 1); // nums[i] taken
+            freq[nums[i]]--;
+        }
+        return result;
+    }
+    
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        sort(nums.begin(), nums.end());
+        return _beautifulSubsets(nums, k, freq, 0) - 1; // -1 for empty subset
+    }
+};
