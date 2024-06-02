73c47e75cca2683796d2b3b9fcb0b00a016d648c
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-06-02 21:19:34 +0530

    Update reversestr.cpp

--- strings/reversestr.cpp
+++ strings/reversestr.cpp
@@ -66,6 +66,13 @@ int main()
 	cout << rev << endl;
 	return 0;
 }
+//June 2 ,2024 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+	int i = 0, j = s.size() - 1;
+	while (i < j) swap(s[i++], s[j--]);
+}
 
 
 
