ffd58289e75a937d57ba94d6527adfc26ab995d0
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-07-04 22:26:49 +0530

    Create mergenodesinbetweenzeroes.cpp

---
+++ Linkedlist/mergenodesinbetweenzeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        for (auto *p_z = head, *p = head->next; p != nullptr; p = p->next) {
+        if (p->val != 0)
+            p_z->val += p->val;
+        else {
+            p_z->next = p->next != nullptr ? p : nullptr;
+            p_z = p;
+        }
+    }
+    return head; 
+    }
+};
