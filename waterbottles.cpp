56722cdef356173f502cccf97986aebac83a1f3a
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-07-07 08:25:30 +0530

    Create waterbottles.cpp

---
+++ waterbottles.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    pair<int, int> divmod(int dividend, int divisor) {
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        return make_pair(quotient, remainder);
+    }
+    int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles;
+        pair<int, int> div = divmod(numBottles, numExchange);
+        int drunk = div.first;
+        int empty = div.second;
+        while(drunk) {
+            result += drunk;
+            div = divmod(drunk + empty, numExchange);
+            drunk = div.first;
+            empty = div.second;
+        }
+        return result;
+    }
+};
