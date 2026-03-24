065c1b2f3951ab359e1d97c2eafb0074e7cba0e2
yash2svi <141433982+yssvi72@users.noreply.github.com>
2026-03-24 23:51:01 +0530

    Fix formatting issues in areaofmaxdiagonal.cpp

--- 2darray/areaofmaxdiagonal.cpp
+++ 2darray/areaofmaxdiagonal.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
         int maxArea = 0, maxDiag = 0;
-
         for (int i = 0; i < dimensions.size(); i++) {
             int l = dimensions[i][0];
             int w = dimensions[i][1];
